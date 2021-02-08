a8ccb683223ef173c49d69241ca96fd3976a2f94
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-07 18:20:07 -0800

    finished connection, moved .gitignore

--- assets/Develop/server.js
+++ assets/Develop/server.js
@@ -9,3 +9,11 @@ const db = mongojs(databaseUrl, collections);
 const app = express();
 
 app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+db.on("error", error => {
+    console.log("Databse Error:", error);
+});
+
