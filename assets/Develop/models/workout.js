58f084b3aeecf7a38cfda1941bec0254ba7c41e5
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-07 19:03:36 -0800

    finished workout model

--- assets/Develop/models/workout.js
+++ assets/Develop/models/workout.js
@@ -5,12 +5,39 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema(
     {
         day: { type: Date, default: () => newDate() },
-        excervises: [
+        excercises: [
             {
                 type: {
                     type: String,
                 },
-            }
-        ]
+                name: {
+                    type: String,
+                },
+                duration: {
+                    type: Number,
+                },
+                weight: Number,
+                reps: Number,
+                sets: Number,
+                distance: Number,
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
     }
-)
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    const duration = this.exercises.reduce((Acc, cur) => {
+        return acc + cur.duration;
+    }, 0);
+
+    return duration;
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
