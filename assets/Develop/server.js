710a7cab0100756a3d86393a5bedb1f8af374ccb
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-07 18:38:15 -0800

    finishing connection code

--- assets/Develop/server.js
+++ assets/Develop/server.js
@@ -4,3 +4,22 @@ cosnt mongoose = require("mongoose");
 const router = require("");
 
 const PORT = preocess.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+let db = moongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    }
+);
