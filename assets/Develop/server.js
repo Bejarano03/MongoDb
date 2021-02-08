9b652af1ece6d70b500391cb0c0790c3159a7997
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-07 18:34:59 -0800

    changing connection

--- assets/Develop/server.js
+++ assets/Develop/server.js
@@ -1,19 +1,6 @@
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
+cosnt mongoose = require("mongoose");
+const router = require("");
 
-const databseUrl = "";
-const collections= [""];
-const db = mongojs(databaseUrl, collections);
-
-const app = express();
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-
-db.on("error", error => {
-    console.log("Databse Error:", error);
-});
-
+const PORT = preocess.env.PORT || 3000;
